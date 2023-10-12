136d731003747cbd2e79db6e8b3214663fee1383
Jake-Pell <jakepell35@gmail.com>
2023-10-12 19:47:05 -0400

    fixed #35

---
+++ src/DataLoader.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class DataLoader {
+
+	public ArrayList<User> getUsers() {
+		return null;
+	}
+
+	public ArrayList<Project> getProjects() {
+		return null;
+	}
+}
