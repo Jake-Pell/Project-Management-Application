40f4cd5f0c5d6b36ad18d4d33a56b4ff31e505bd
Jake-Pell <jakepell35@gmail.com>
2023-10-15 22:15:35 -0400

    Added data constants for User, #15

--- src/DataCostants.java
+++ src/DataCostants.java
@@ -1,3 +1,8 @@
 public abstract class DataConstants {
-	
+	protected final String USER_FILE_NAME = "users.json";
+	protected final String USER_ID = "id";
+	protected final String USER_FIRST_NAME = "firstName";
+	protected final String USER_LAST_NAME = "lastName";
+	protected final String USER_UNAME = "username";
+	protected final String USER_PASSWORD = "password";
 }
