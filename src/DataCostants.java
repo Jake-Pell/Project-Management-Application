d4ebd18dd1c528d719ae09b61690d3ff0881bb61
Jake-Pell <jakepell35@gmail.com>
2023-10-15 22:27:08 -0400

    updated User json file path, #15

--- src/DataCostants.java
+++ src/DataCostants.java
@@ -1,5 +1,5 @@
 public abstract class DataConstants {
-	protected final String USER_FILE_NAME = "users.json";
+	protected final String USER_FILE_NAME = "../lib/json/users.json";
 	protected final String USER_ID = "id";
 	protected final String USER_FIRST_NAME = "firstName";
 	protected final String USER_LAST_NAME = "lastName";
