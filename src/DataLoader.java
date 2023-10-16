fe14e3c8ed2aec1421685e3a67c9569b39653f4c
Arshia Eslami <arshia.esl83@gmail.com>
2023-10-16 09:41:40 -0400

    update on #12

--- src/DataLoader.java
+++ src/DataLoader.java
@@ -1,9 +1,16 @@
 // Copyright 2023 Cocky 4
-import java.util.ArrayList;
+import java.util.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 
 public class DataLoader {
 
+
 	public static ArrayList<User> getUsers() {
+		ArrayList<User> users = new ArrayList<User>();
+
 		return null;
 	}
 
