94c0ae9c7fc45080a3faac46c414050c5347e7af
Jake-Pell <jakepell35@gmail.com>
2023-10-16 01:05:49 -0400

    updated user constants to be static, #15

--- src/DataConstants.java
+++ src/DataConstants.java
@@ -1,11 +1,11 @@
 // Copyright 2023 Cocky 4
 
 public abstract class DataConstants {
-	protected final String USER_FILE_NAME = "../lib/json/users.json";
-	protected final String USER_ID = "id";
-	protected final String USER_FIRST_NAME = "firstName";
-	protected final String USER_LAST_NAME = "lastName";
-	protected final String USER_UNAME = "username";
-	protected final String USER_PASSWORD = "password";
-	protected final String USER_PROJECTS = "projects";
+	protected final static String USER_FILE_NAME = "../lib/json/users.json";
+	protected final static String USER_ID = "id";
+	protected final static String USER_FIRST_NAME = "firstName";
+	protected final static String USER_LAST_NAME = "lastName";
+	protected final static String USER_UNAME = "username";
+	protected final static String USER_PASSWORD = "password";
+	protected final static String USER_PROJECTS = "projects";
 }
