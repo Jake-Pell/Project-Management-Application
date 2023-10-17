c1e1a25503fa31855091cc3339adaa391fb61799
Arshia Eslami <arshia.esl83@gmail.com>
2023-10-17 13:54:00 -0400

    fixed #7

--- src/History.java
+++ src/History.java
@@ -1,3 +1,14 @@
+// Copyright 2023 Cocky 4 
+
+import java.util.Date;
+
 public class History {
-    
+   
+    private Date date;
+    private String newChange; 
+
+    public History(Date date, String newChange) {
+
+    }
+
 }
