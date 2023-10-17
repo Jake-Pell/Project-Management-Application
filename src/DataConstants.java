0311b261e2cfdf59ea86cb5d28670a789ddf68d2
Arshia Eslami <arshia.esl83@gmail.com>
2023-10-17 14:19:18 -0400

    changing constants

--- src/DataConstants.java
+++ src/DataConstants.java
@@ -3,7 +3,7 @@
 public abstract class DataConstants {
   protected final static String ID = "id";
   // User
-	protected final static String USER_FILE_NAME = "../json/users.json";
+	protected final static String USER_FILE_NAME = "json/users.json";
 	protected final static String USER_FIRST_NAME = "firstName";
 	protected final static String USER_LAST_NAME = "lastName";
 	protected final static String USER_UNAME = "username";
@@ -11,7 +11,7 @@ public abstract class DataConstants {
 	protected final static String USER_PROJECTS = "projects";
 
   // Project
-  protected final static String PROJECT_FILE_NAME = "../json/projects.json";
+  protected final static String PROJECT_FILE_NAME = "json/projects.json";
   protected final static String PROJECT_NAME = "name";
   protected final static String PROJECT_USERS = "users";
   protected final static String PROJECT_COLUMNS = "columns";
