d6f4e18591febb9cc8c3fa14de2fb81dea1a9a26
Arshia Eslami <arshia.esl83@gmail.com>
2023-10-17 13:29:16 -0400

    edit user

--- src/User.java
+++ src/User.java
@@ -8,7 +8,6 @@ public class User {
 	private String lastName;
 	private String username;
 	private String password;
-	private ArrayList<Project> projects;
 
 	public User(String firstName, String lastName, String username, String password) {
 		this.firstName = firstName;
@@ -16,56 +15,6 @@ public class User {
 		this.username = username;
 		this.password = password;
 	}
-
-	public ArrayList<Project> getProjects() {
-		return projects;
-	}
-
-	public boolean addProject(String projectName) {
-		return true;
-	}
-
-	public boolean editProjectName(String newName) {
-		return true;
-	}
-
-	public boolean addColumn(String columnName) {
-		return true;
-	}
-
-	public boolean editColumnName(Column column, String columnName) {
-		return true;
-	}
-
-	public boolean moveColumn(Column column, Direction direction) {
-		return true;
-	}
-
-	public boolean createTask(Column column, String taskName, String description,
-								int priority) {
-		return true;
-	}
-
-	public boolean editTaskDescription(Task task, String description) {
-		return true;
-	}
-
-	public boolean editTaskPriority(Task task, int priority) {
-		return true;
-	}
-
-	public boolean editTaskName(Task task, String name) {
-		return true;
-	}
-
-	public boolean addComment(Project project, String description) {
-		return true;
-	}
-
-	public boolean addComment(Task task, String description) {
-		return true;
-	}
-
 	public UUID getID() {
 		return id;
 	}
