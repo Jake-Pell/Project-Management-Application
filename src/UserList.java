954bd104109499300b8e8a1e8cbe5cf83a3b21b8
Jake-Pell <jakepell35@gmail.com>
2023-10-18 01:23:47 -0400

    fixed #59

--- src/UserList.java
+++ src/UserList.java
@@ -23,6 +23,14 @@ public class UserList {
 		return users.add(new User(firstName, lastName, username, password));
 	}
 
+	public static User getUser(String username, String password) {
+		for (User u : users) {
+			if (u.getUsername().equals(username) && u.getPassword().equals(password))
+				return u;
+		}
+		return null;
+	}
+
 	public ArrayList<User> getUsers() {
 		return users;
 	}
