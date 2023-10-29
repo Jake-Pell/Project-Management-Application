3987d868f76b0de47fa457ffc468da779b0b2007
Jake-Pell <jakepell35@gmail.com>
2023-10-28 22:33:19 -0400

    updated Task constructor, #23

--- src/Task.java
+++ src/Task.java
@@ -13,6 +13,8 @@ public class Task {
     this.taskName = taskName;
     this.description = description;
     this.priority = priority;
+    users = new ArrayList<User>();
+    comments = new ArrayList<Comment>();
   }
   
 
