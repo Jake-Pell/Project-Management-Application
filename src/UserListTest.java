17c96e1f591b2cf72cd108b2abbdbf7325f8c883
Jake-Pell <jakepell35@gmail.com>
2023-11-02 14:30:02 -0400

    added methods for before and after each test, #77

---
+++ src/UserListTest.java
@@ -0,0 +1,23 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.api.Test;
+import java.util.ArrayList;
+
+class UserListTest {
+    private UserList userList = UserList.getInstance();
+    private ArrayList<User> users = userList.getUsers();
+
+    @BeforeEach
+    public void setup() {
+        users.clear();
+        users.add(new User("Jake", "Pell", "pellj", "password123"));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        UserList.getInstance().getUsers().clear();
+        DataWriter.saveUsers();
+    }
+    
+}
