561ebb9efbe733ecd497ff6b736cfd00e0e1fb7e
joshtd07 <jdoyon@email.sc.edu>
2023-11-08 22:15:53 -0500

    Updated DataLoader Test

--- src/DataLoaderTest.java
+++ src/DataLoaderTest.java
@@ -1,46 +1,43 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 class DataLoaderTest {
-	private Users users = Users.getInstance();
-	private ArrayList<User> userList = users.getUsers();
-	
-	@BeforeEach
-	public void setup() {
-		userList.clear();
-		userList.add(new User("asmith", "Amy", "Smith", 19, "803-454-3344"));
-		userList.add(new User("bwhite", "Bob", "White", 23, "803-333-3544"));
-		DataWriter.saveUsers();
-	}
-	
-	@AfterEach
-	public void tearDown() {
-		Users.getInstance().getUsers().clear();
-		DataWriter.saveUsers();
-	}
-	
-	
-	@Test
-	void testGetUsersSize() {
-		userList = DataLoader.getUsers();
-		assertEquals(2, userList.size());
-	}
+    private ArrayList<User> userList;
 
-	@Test
-	void testGetUsersSizeZero() {
-		Users.getInstance().getUsers().clear();
-		DataWriter.saveUsers();
-		assertEquals(0, userList.size());
-	}
-	
-	@Test
-	void testGetUserFirstUserName() {
-		userList = DataLoader.getUsers();
-		assertEquals("asmith", userList.get(0).getUserName());
-	}
+    @BeforeEach
+    public void setup() {
+        userList = DataLoader.getUsers();
+        userList.add(new User("asmith", "Amy", "Smith", "19", "803-454-3344"));
+        userList.add(new User("bwhite", "Bob", "White", "23", "803-333-3544"));
+        DataWriter.saveUsers();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Users.getInstance().getUsers().clear();
+        DataWriter.saveUsers();
+    }
+
+    @Test
+    void testGetUsersSize() {
+        userList = DataLoader.getUsers();
+        assertEquals(2, userList.size());
+    }
+
+    @Test
+    void testGetUsersSizeZero() {
+        Users.getInstance().getUsers().clear();
+        DataWriter.saveUsers();
+        assertEquals(0, userList.size());
+    }
+
+    @Test
+    void testGetUserFirstUserName() {
+        userList = DataLoader.getUsers();
+        assertEquals("asmith", userList.get(0).getUserName());
+    }
 }
